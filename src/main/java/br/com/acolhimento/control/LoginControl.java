3a5c8af837d882ebfb6ece23cb9af4c128067cbe
GabrielFFranco <42790738+GabrielFFranco@users.noreply.github.com>
2018-11-13 20:48:54 -0200

    Add files via upload

---
+++ src/main/java/br/com/acolhimento/control/LoginControl.java
@@ -0,0 +1,16 @@
+package br.com.acolhimento.control;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+@ManagedBean(name = "loginControl", eager = true)
+public class LoginControl {
+	
+	String login;
+	String senha;
+	
+	public void validarLogin() {
+		
+	}
+	
+}
